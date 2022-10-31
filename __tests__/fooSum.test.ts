f7acab06bff82538b5eb64f4b90485bdd12ac622
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2022-10-31 10:25:55 +0100

    Add simple function test

---
+++ __tests__/fooSum.test.ts
@@ -0,0 +1,5 @@
+import fooSum from "../fooSum";
+
+test('adds 1 + 2 to equal 3', () => {
+    expect(fooSum(1, 2)).toBe(3);
+});
---
+++ fooSum.ts
@@ -0,0 +1,5 @@
+function fooSum(a: number, b: number) {
+  return a + b;
+}
+
+export default fooSum;
