d0fcc0d202d1db7d04869dd5ab2042c5c8e03d23
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2022-11-02 19:50:22 +0100

    Add Reactotron Redux logs

--- redux/store.ts
+++ redux/store.ts
@@ -1,8 +1,12 @@
 import { createStore, combineReducers } from 'redux';
 import CountReducer from './reducers/countReducer';
+import Reactotron from '../ReactotronConfig';
 
 const rootReducer = combineReducers({
     count: CountReducer,
 });
 
-export const store = createStore(rootReducer);
+export const store = createStore(
+    rootReducer,
+    Reactotron.createEnhancer()
+);
