24cf40896922193b31819638646bd799da7a8db8
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2022-11-03 13:09:30 +0100

    Add Redux Flipper Debugger

--- redux/store.ts
+++ redux/store.ts
@@ -1,4 +1,4 @@
-import { createStore, combineReducers } from 'redux';
+import { createStore, combineReducers, applyMiddleware, compose } from 'redux';
 import CountReducer from './reducers/countReducer';
 import Reactotron from '../ReactotronConfig';
 
@@ -6,7 +6,29 @@ const rootReducer = combineReducers({
     count: CountReducer,
 });
 
+const middlewares = [
+    /* other middlewares */
+];
+
+if (__DEV__) {
+    const createDebugger = require('redux-flipper').default;
+    middlewares.push(createDebugger());
+    Reactotron.log?.("redux-flipper debugger middelware created")
+}
+
+const middlewareEnhancer = applyMiddleware(...middlewares);
+const reactotronEnhancers = Reactotron.createEnhancer?.() 
+var composedEnhancers = middlewareEnhancer
+if (reactotronEnhancers != undefined) {
+    composedEnhancers = compose(composedEnhancers, reactotronEnhancers);
+    Reactotron.log?.(composedEnhancers)
+} 
+
+// - https://github.com/jk-gan/flipper-plugin-redux-debugger/issues/10
+// - https://github.com/jk-gan/redux-flipper
+// - https://fbflipper.com/docs/features/react-native/#community-react-native-plugins-for-flipper
+
 export const store = createStore(
     rootReducer,
-    Reactotron.createEnhancer()
+    composedEnhancers
 );
