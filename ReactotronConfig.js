673219344317568d143f975b18be6510c1e0a28f
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2022-11-03 14:41:32 +0100

    Try to configure reactotron-flipper-plugin but with no luck

--- ReactotronConfig.js
+++ ReactotronConfig.js
@@ -1,9 +1,15 @@
 import Reactotron, { networking } from 'reactotron-react-native'
 import { reactotronRedux } from 'reactotron-redux'
+// https://github.com/infinitered/reactotron-react-native/issues/492
+// @ts-ignore
+import FlipperConnectionManager from 'reactotron-react-native/dist/flipper'
 
 const reactotron = Reactotron
   //.setAsyncStorageHandler(AsyncStorage) // AsyncStorage would either come from `react-native` or `@react-native-community/async-storage` depending on where you get it from
-  .configure() // controls connection & communication settings
+  .configure({
+    //createSocket: path => new FlipperConnectionManager(path),
+    //host: "192.168.1.153"
+  }) // controls connection & communication settings
   .useReactNative() // add all built-in react native plugins
   .use(reactotronRedux()) // add redux support
   .use(networking())
