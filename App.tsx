bb523ec4891ccae559c21ef5f74a02fd6f8b8e2c
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2022-12-20 11:09:06 +0100

    test io-react-native-crypto

--- App.tsx
+++ App.tsx
@@ -7,15 +7,20 @@
  *
  * @format
  */
-
-import React, { useEffect, useState, type PropsWithChildren } from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
-import Reactotron from 'reactotron-react-native'
+import {
+  generate,
+  sign,
+  getPublicKey,
+  deleteKey,
+  CryptoError,
+} from '@pagopa/io-react-native-crypto';
+import React, {useEffect, type PropsWithChildren} from 'react';
+import {NavigationContainer} from '@react-navigation/native';
+import {createStackNavigator} from '@react-navigation/stack';
+import Reactotron from 'reactotron-react-native';
 
 import {
-  SafeAreaView,
-  ScrollView,
+  // eslint-disable-next-line @typescript-eslint/no-unused-vars
   StatusBar,
   StyleSheet,
   Text,
@@ -25,23 +30,17 @@ import {
   Pressable,
 } from 'react-native';
 
-import {
-  Colors,
-  DebugInstructions,
-  Header,
-  LearnMoreLinks,
-  ReloadInstructions,
-} from 'react-native/Libraries/NewAppScreen';
-import { Provider, useDispatch, useSelector } from 'react-redux';
-import { store } from './redux/store';
-import { decrement, increment } from './redux/actions/counterAction';
+import {Colors} from 'react-native/Libraries/NewAppScreen';
+import {Provider, useDispatch, useSelector} from 'react-redux';
+import {store} from './redux/store';
+import {decrement, increment} from './redux/actions/counterAction';
 import moment from 'moment';
 
 const Section: React.FC<
   PropsWithChildren<{
     title: string;
   }>
-> = ({ children, title }) => {
+> = ({children, title}) => {
   const isDarkMode = useColorScheme() === 'dark';
   return (
     <View style={styles.sectionContainer}>
@@ -67,9 +66,9 @@ const Section: React.FC<
   );
 };
 
-export function HomeScreen({ navigation }) {
+export function HomeScreen({navigation}) {
   const dispatch = useDispatch();
-  const count = useSelector((store) => store.count.count);
+  const count = useSelector(store => store.count.count);
   const handleIncreament = () => {
     dispatch(increment());
   };
@@ -78,26 +77,67 @@ export function HomeScreen({ navigation }) {
     dispatch(decrement());
   };
 
+  async function generateKey() {
+    try {
+      const result = await generate('PERSONAL_KEYTAG');
+      console.log(result);
+    } catch (e) {
+      const {message, userInfo} = e as CryptoError;
+      console.log(message);
+      console.log(userInfo);
+    }
+  }
+
+  async function signWithKey() {
+    try {
+      const result = await sign('A valid message to sign', 'PERSONAL_KEYTAG');
+      // result is the Base64 string representation of the signature.
+      console.log('-> ' + JSON.stringify(result));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  async function getKey() {
+    try {
+      const result = await getPublicKey('PERSONAL_KEYTAG');
+      console.log('-> ' + JSON.stringify(result));
+    } catch (e) {
+      console.log(e);
+    }
+  }
+  async function deleteK() {
+    try {
+      const result = await deleteKey('PERSONAL_KEYTAG');
+      console.log('-> ' + JSON.stringify(result));
+    } catch (e) {
+      console.log(e);
+    }
+  }
   const fetchRequest = () => {
     fetch('https://dummyjson.com/products', {
       method: 'GET',
       headers: {
         Accept: 'application/json',
-        'Content-Type': 'application/json'
-      }
-    }).then((response) => response.json())
-      .then((json) => {
-        reactotronLog?.(json)
+        'Content-Type': 'application/json',
+      },
+    })
+      .then(response => response.json())
+      .then(json => {
+        reactotronLog?.(json);
       })
-      .catch((error) => {
+      .catch(error => {
         console.error(error);
-      });;
+      });
   };
 
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <View style={styles.container}>
         <Text>Home Screen</Text>
+        <Button title="generateKey" onPress={generateKey} testID="key gen" />
+        <Button title="signWithKey" onPress={signWithKey} testID="key gen" />
+        <Button title="getPublicKey" onPress={getKey} testID="key gen" />
+        <Button title="deleteKey" onPress={deleteK} testID="key gen" />
         <Button
           title="Test HTTP Request"
           onPress={fetchRequest}
@@ -109,16 +149,20 @@ export function HomeScreen({ navigation }) {
           testID="button"
         />
         <Text style={styles.title_text}>Counter App</Text>
-        <Text style={styles.counter_text} testID="counter">{count}</Text>
-        <Pressable testID="incrementButton"
+        <Text style={styles.counter_text} testID="counter">
+          {count}
+        </Text>
+        <Pressable
+          testID="incrementButton"
           onPress={handleIncreament}
           style={styles.btn}>
           <Text style={styles.btn_text}> Increment </Text>
         </Pressable>
 
-        <Pressable testID="decrementButton"
+        <Pressable
+          testID="decrementButton"
           onPress={handleDecreament}
-          style={{ ...styles.btn, backgroundColor: '#6e3b3b' }}>
+          style={{...styles.btn, backgroundColor: '#6e3b3b'}}>
           <Text style={styles.btn_text}> Decrement </Text>
         </Pressable>
       </View>
@@ -174,7 +218,7 @@ const styles = StyleSheet.create({
 
 function DetailsScreen() {
   return (
-    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
       <Text>Details Screen</Text>
     </View>
   );
@@ -186,75 +230,77 @@ const reactotronWarn = Reactotron.warn;
 const reactotronError = Reactotron.error;
 
 function pad(n: number): string {
-  return n.toString().padStart(2, "0");
+  return n.toString().padStart(2, '0');
 }
 
-function formatDateForFiscalCode(
-  d: Date
-): Date {
+function formatDateForFiscalCode(d: Date): Date {
   const year = d.getUTCFullYear();
   const month = pad(d.getUTCMonth() + 1);
   const day = pad(d.getUTCDate());
-  const now = d//new Date();
-  console.log("💥 " + now.toISOString());
+  const now = d; //new Date();
+  console.log('💥 ' + now.toISOString());
   const hours = pad(now.getUTCHours());
   const minutes = pad(now.getUTCMinutes());
   const seconds = pad(now.getUTCSeconds());
-  return new Date(
-    `${year}-${month}-${day}T${hours}:${minutes}:${seconds}.000`
-  );
+  return new Date(`${year}-${month}-${day}T${hours}:${minutes}:${seconds}.000`);
 }
 
 function App() {
   useEffect(() => {
     const iso8601String = '1977-05-22T00:00:00.000Z';
     const myDate = new Date(iso8601String);
-    const utcByMoment = moment(myDate).utc().format("DD/MM/YYYY");
-    console.log("********* moment **********");
+    const utcByMoment = moment(myDate).utc().format('DD/MM/YYYY');
+    console.log('********* moment **********');
     console.log(myDate.toISOString());
     console.log(myDate.toString());
-    console.log("🦄 " + utcByMoment);
-    console.log("********* std **********");
+    console.log('🦄 ' + utcByMoment);
+    console.log('********* std **********');
     const uDate = formatDateForFiscalCode(myDate);
     console.log(uDate.toISOString());
     console.log(uDate.toString());
-    console.log("🚀 " + uDate.toLocaleDateString(
-      "it-IT", {
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-      //hour12: false,
-      //hour: "2-digit",
-      //minute: "2-digit"
-    })
+    console.log(
+      '🚀 ' +
+        uDate.toLocaleDateString('it-IT', {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+          //hour12: false,
+          //hour: "2-digit",
+          //minute: "2-digit"
+        }),
     );
-    console.log("🚀🚀 " + myDate.toLocaleDateString(
-      "it-IT", {
-      year: "numeric",
-      month: "numeric",
-      day: "numeric",
-      //hour12: false,
-      //hour: "2-digit",
-      //minute: "2-digit"
-    })
+    console.log(
+      '🚀🚀 ' +
+        myDate.toLocaleDateString('it-IT', {
+          year: 'numeric',
+          month: 'numeric',
+          day: 'numeric',
+          //hour12: false,
+          //hour: "2-digit",
+          //minute: "2-digit"
+        }),
     );
     // https://bobbyhadz.com/blog/typescript-cannot-invoke-an-object-which-is-possibly-undefined#:~:text=The%20error%20%22Cannot%20invoke%20an,(%3F.)%2C%20e.g.%20employee.
     reactotronLog?.('Rendering App Component!');
-    reactotronLog?.({ numbers: [1, 2, 3], boolean: false, nested: { here: 'we go' } });
+    reactotronLog?.({
+      numbers: [1, 2, 3],
+      boolean: false,
+      nested: {here: 'we go'},
+    });
     reactotronWarn?.('*glares*');
     reactotronError?.('Now you have done it!', null);
     Reactotron.display({
       name: 'KNOCK KNOCK',
-      preview: 'Who\'s there?',
-      value: 'Orange.'
-    })
+      preview: "Who's there?",
+      value: 'Orange.',
+    });
 
     Reactotron.display({
       name: 'ORANGE',
       preview: 'Who?',
-      value: 'Orange you glad you don\'t know me in real life?',
-      important: true
-    })
+      value: "Orange you glad you don't know me in real life?",
+      important: true,
+    });
   });
 
   return (
