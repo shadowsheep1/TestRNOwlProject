f49355dc6850cc024361604692d6991f6ce0809c
Fabio Bombardi <16268789+shadowsheep1@users.noreply.github.com>
2023-02-05 17:19:43 +0100

    d

--- scripts/avd-create.cmd
+++ scripts/avd-create.cmd
@@ -1,1 +1,1 @@
-%ANDROID_HOME%\cmdline-tools\latest\bin\avdmanager.bat create avd -n Pixel_API_31 --device "Nexus 5X" --package "system-images;android-30;default;x86_64" --sdcard 512M
+%ANDROID_HOME%\cmdline-tools\latest\bin\avdmanager.bat create avd -n Pixel_API_31 --device 'Nexus 5' --package "system-images;android-30;default;x86_64" --sdcard 512M
